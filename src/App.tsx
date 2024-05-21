7a5fba1c606876de2c6d6197168136c6fe5bf163
Michael Farquhar <michaelfarquhar4@gmail.com>
2024-05-21 00:10:45 -0400

    Redesign footer and restructure file structure of footer and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { Footer } from './Footer';
-import { Header } from './Header';
+import { Footer } from './layouts/footer/Footer';
+import { Header } from './layouts/header/Header';
 import { RandomKana } from './RandomKana';
 
 export default function App() {
