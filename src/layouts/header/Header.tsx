05b149a533d8d9f35615553510d62a169b423658
Michael Farquhar <michaelfarquhar4@gmail.com>
2024-07-02 15:21:46 -0400

    Update logos, favicon and manfest.json

--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -1,21 +1,13 @@
 export const Header = () => {
     return (
         <header className="w-full py-4 border-b flex items-center justify-center shadow-md bg-white">
-            <div className="flex items-center space-x-2">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    className="w-10 h-10 "
-                >
-                    <path
-                        fill="none"
-                        stroke="#58b4ff"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M5 5h6.586a1 1 0 0 1 .707 1.707L11 8M8 8c0 1.5.5 3-2 5m6 7l4-9l4 9m-.9-2h-6.2"
-                    ></path>
-                </svg>
+            <div className="flex items-center space-x-3">
+                <img
+                    src="/icons/favicon-32x32.png"
+                    width={32}
+                    height={32}
+                    alt="App logo"
+                />
                 <div className="font-logo font-bold text-4xl text-gray-800">
                     Random Kana
                 </div>
