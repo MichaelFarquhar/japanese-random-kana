fe4660a3a128d25d94617a40b2846d6259e2c25a
Michael Farquhar <michaelfarquhar4@gmail.com>
2025-10-21 15:55:18 -0400

    Cleanup Footer

--- src/layouts/footer/Footer.tsx
+++ src/layouts/footer/Footer.tsx
@@ -1,38 +1,34 @@
 export const Footer = () => {
-    return (
-        <footer className="bg-white w-full">
-            <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row items-center">
-                <p className="text-gray-500 text-sm text-center sm:text-left">
-                    © {new Date().getFullYear()} Random Kana —
-                    <a
-                        className="text-blue-400 font-medium ml-1 transition hover:text-blue-600"
-                        href="https://wanakana.com/"
-                        rel="noopener noreferrer"
-                        target="_blank"
-                    >
-                        Powered by Wanakana
-                    </a>
-                </p>
-                <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-                    <a
-                        className="text-gray-700 transition hover:text-blue-600"
-                        href="https://github.com/MichaelFarquhar/japanese-random-kana"
-                        rel="noopener noreferrer"
-                        target="_blank"
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            className="w-6 h-6"
-                        >
-                            <path
-                                fill="currentColor"
-                                d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33s1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2"
-                            />
-                        </svg>
-                    </a>
-                </span>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="bg-white w-full">
+      <div className="py-4 px-5 flex flex-wrap items-center justify-between">
+        <p className="text-gray-500 text-sm text-center sm:text-left">
+          © {new Date().getFullYear()} — Powered by
+          <a
+            className="text-blue-400 font-medium transition hover:text-blue-600"
+            href="https://wanakana.com/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            &nbsp;Wanakana
+          </a>
+        </p>
+        <span className="inline-flex sm:ml-auto justify-center">
+          <a
+            className="text-gray-700 transition hover:text-blue-600"
+            href="https://github.com/MichaelFarquhar/japanese-random-kana"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-6 h-6">
+              <path
+                fill="currentColor"
+                d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33s1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2"
+              />
+            </svg>
+          </a>
+        </span>
+      </div>
+    </footer>
+  );
 };
